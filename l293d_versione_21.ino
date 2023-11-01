49858b9a249e1fc9346d6e30689671eef50036d7
Ing. Di Filippo <davidedifilippo@gmail.com>
2023-11-01 10:31:06 +0100

    versione funzionante su tinkercad

--- l293d_versione_21.ino
+++ l293d_versione_21.ino
@@ -1,13 +1,15 @@
-const int enable = 11;
-const int controllo_ramo_A = 8; //Devono sempre essere uno il complemento dell'altro
-const int controllo_ramo_B = 7;
+const int enablePin = 11;
 
-const int progButton = 5;
-const int upButton = 4;
-const int downButton = 3;
+const int pinControllo_ramo_sx = 8; 
+const int pinControllo_ramo_dx = 7;
+
+const int progButton = 4;
+const int upButton = 3;
+const int downButton = 2;
 
 int cw = 1; //variabile di controllo della direzione -> orario predefinito
 int numeroSpire = 5000;
+
 int giriMinuto = 0;
 
 long int tempoAvvio = 0;
@@ -17,53 +19,95 @@ int giriEffettuati = 0;
 
 void setup()
 {
-	pinMode(enable,OUTPUT);
-  	pinMode(controllo_ramo_A, OUTPUT);
-    pinMode(controllo_ramo_B, OUTPUT);
+    pinMode(enablePin,OUTPUT);
+    pinMode(pinControllo_ramo_sx, OUTPUT);
+    pinMode(pinControllo_ramo_dx, OUTPUT);
   
     pinMode(progButton,INPUT);
-  	pinMode(upButton, INPUT);
+    pinMode(upButton, INPUT);
     pinMode(downButton, INPUT);
   
+  
     Serial.begin(9600);
+    Serial.println("Setup macchina avvolgitrice! ");
+ 
+   //se non si preme prg si resta nel loop di interrogazione 
+   //dei pulsanti up e down
+   Serial.println("Setup numero avvolgimenti: ");
+ 
+  while(!digitalRead(progButton)){
+    if (digitalRead(upButton)) { //se premo up
+      numeroSpire = numeroSpire +100;
+      Serial.println(numeroSpire);
+    } 
+    if (digitalRead(downButton)) { //se premo down
+      numeroSpire = numeroSpire -100;
+      Serial.println(numeroSpire);
+    }
+    delay(100);
+  }
+  delay(100); //evita di saltare il blocco successivo a causa dei rimbalzi del pulsante prog
+ 
+  Serial.println("Setup verso di rotazione: ");
+  while(!digitalRead(progButton)){
+    if (digitalRead(upButton)) { //se premo up
+      cw = 0; //verso antiorario
+      Serial.println(cw);
+    } 
+    if (digitalRead(downButton)) { //se premo down
+      cw=1;	//verso orario
+      Serial.println(cw);
+    }
+    delay(100);
+  }
   
-    Serial.println("Ready");
-  
-   //se non si preme prg si rest nel loop di interrogazione dei pulsanti up e down
   
    if(cw != 0)
-   {digitalWrite(controllo_ramo_A, HIGH);
-    digitalWrite(controllo_ramo_B, LOW);
+   {digitalWrite(pinControllo_ramo_sx, HIGH);
+    digitalWrite(pinControllo_ramo_dx, LOW);
    }
    else
-   {digitalWrite(controllo_ramo_A, LOW);
-    digitalWrite(controllo_ramo_B,HIGH);
+   {
+    digitalWrite(pinControllo_ramo_sx, LOW);
+    digitalWrite(pinControllo_ramo_dx,HIGH);
    }
   
     //avvio il motore con la velocità e il verso impostati
   
     int pot = analogRead(A0);
+    Serial.print(pot); //imposto al 50% ad esempio
   
-  	giriMinuto = (analogRead(A0)/1023.0)*1000;
+    delay(2000);
+  
+    giriMinuto = (pot/1023.0)*1000; //suppongo 1000 rpm velocità massima di rotazione 
     
-  	tempoAvvio = millis();
+    int duty = (giriMinuto/1000.0)*255; 
+   
+    //Il duty cycle del segnale PWM determina la tensione di alimentazione motore: 
+    //duty = 255 ->motore 1000 rpm
+    //duty = 128 -> motore 500 rpm
     
+    Serial.print("giriMinuto: ");
     Serial.println(giriMinuto);
-    Serial.println((giriMinuto/1000.0)*255);
+    Serial.print("Duty Cycle: ");
+    Serial.println(duty);
     
-  	analogWrite(enable, (giriMinuto/1000.0)*255);
-  
-  
+    delay(1000);
+    
+    analogWrite(enablePin, (giriMinuto/1000.0)*255); //Imposto il pwm al valore desiderato
+  	tempoAvvio = millis();  
+    
 }
 
 void loop()
 {
-  giriEffettuati = (giriMinuto/60.0) * ((millis()-tempoAvvio)/1000.0);
+  giriEffettuati = (giriMinuto/60.0) * ((millis()-tempoAvvio)/1000.0); //giri/secondo * secondi = giri fino ad ora
   Serial.println(giriEffettuati);
   if(giriEffettuati >= numeroSpire)
   {
-    analogWrite(enable,0);
-   setup();
+    analogWrite(enablePin,0); //Ho finito. Arresto motore cc
+    delay(1000);
+    setup(); //Torno all'inizio
   }
  
 }
